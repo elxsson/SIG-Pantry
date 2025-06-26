05fd9f5f4ea3c1e5d518d049ff88f02853427812
elxsson <soullcakezz@gmail.com>
2025-06-26 20:40:47 -0300

    feat: add service de gerenciamento de itens

---
+++ app/service/items.py
@@ -0,0 +1,20 @@
+# CRUD de itens
+
+def add_item():
+    print("\n[Adicionar Item]")
+
+
+def remove_item():
+    print("\n[Remover Item]")
+
+
+def update_item():
+    print("\n[Atualizar Item]")
+
+
+def list_items():
+    print("\n[Listar Itens]")
+
+
+def search_item():
+    print("\n[Buscar Item]")

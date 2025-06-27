88503efdd03d4b52aa4aec6def13333b46d3000e
elxsson <soullcakezz@gmail.com>
2025-06-26 22:02:44 -0300

    feat: add menu de itens
    o menu de itens contem todas as actions que serao usadas para manipular o modulo de itens da despensa

--- app/menu.py
+++ app/menu.py
@@ -1,6 +1,44 @@
 import inquirer
 from app import service
 
+
+def show_items_menu():
+    questions = [
+        inquirer.List(
+            "option",
+            message="Menu Itens - Selecione uma ação:",
+            choices=[
+                "Adicionar item",
+                "Remover item",
+                "Atualizar item",
+                "Listar itens",
+                "Buscar item por nome",
+                "Voltar"
+            ],
+        )
+    ]
+    answer = inquirer.prompt(questions)
+    if not answer:
+        return False
+
+    option = answer["option"]
+
+    if option == "Adicionar item":
+        service.add_item()
+    elif option == "Remover item":
+        service.remove_item()
+    elif option == "Atualizar item":
+        service.update_item()
+    elif option == "Listar itens":
+        service.list_items()
+    elif option == "Buscar item por nome":
+        service.search_item()
+    elif option == "Voltar":
+        return False
+
+    return True
+
+
 def show_main_menu():
     running = True
 
